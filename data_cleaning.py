8536797bbbe6bcc1c858a1dbdefb5d01d0c627cd
willie3838 <williamchan@rogers.com>
2020-12-02 20:32:02 -0500

    Add files via upload

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,115 +9,145 @@
 import pandas as pd
 import nltk
 
-# reading the csv file
-df = pd.read_csv("April_23/coronavirus-tweet-id-2020-04-23-00.csv")
+def clean_data(filename : str):
 
-################# UNECESSARY COLUMNS ##################
-
-df.drop(columns=['place','possibly_sensitive','user_created_at','user_default_profile_image',
-                 'user_favourites_count','user_listed_count','user_name','user_time_zone',
-                 'user_urls'], inplace=True)
+    # reading the csv file
+    df = pd.read_csv(filename,error_bad_lines=False, engine="python")
 
 
-################## REMOVING VERIFIED ACCOUNTS ##############
-df = df[df['user_verified'] == False]
+    ################# UNECESSARY COLUMNS ##################
 
-# creating a temporary dataframe to store cleaned data
-temp_df = pd.DataFrame()
-
-# make all text lower case
-df.text = df.text.str.lower()
-
-################# REMOVING RANDOM CHARACTERS ##################
-
-# uses regex and removes all characters that arent part of the alphabet, number set, puncation set, and special character set
-df.text = df.text.str.replace(r'[^a-z0-9~`!@#$%^&*()_+[{\]\}\|;:\'\",<.>/? ]+','')
-
-################# LOCATION DATA ##################
-
-# finding rows that  contain the word "Canada" in user_location
-# dropping all NA/NaN values in the column user_location
-df.dropna(subset=['user_location'],inplace=True)
-
-###### COUNTRY SEMANTICS #######
-# adding all data that include 'Canada' in user_location to the new dataframe
-temp_df = temp_df.append(df[df['user_location'].str.contains('canada')])
-
-##### PROVINCE ANALYSIS ########
-# Using the full name of the province + internationally approved abbreviations
-
-## NEWFOUNDLAND AND LABRADOR ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('newfoundland and labrador')])
+    df.drop(columns=['place','possibly_sensitive','user_created_at','user_default_profile_image',
+                     'user_favourites_count','user_listed_count','user_name','user_time_zone',
+                     'user_urls'], inplace=True)
 
 
-## PRINCE EDWARD ISLAND ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('prince edward island')])
+    ################## REMOVING VERIFIED ACCOUNTS ##############
+    df = df[df['user_verified'] == False]
+
+    # creating a temporary dataframe to store cleaned data
+    temp_df = pd.DataFrame()
+
+    # make all text lower case
+    df.text = df.text.str.lower()
+
+    ################# REMOVING RANDOM CHARACTERS ##################
+
+    # uses regex and removes all characters that arent part of the alphabet, number set, puncation set, and special character set
+    df.text = df.text.str.replace(r'[^a-z0-9~`!@#$%^&*()_+[{\]\}\|;:\'\",<.>/? ]+','')
+
+    ################# LOCATION DATA ##################
+
+    # finding rows that  contain the word "Canada" in user_location
+    # dropping all NA/NaN values in the column user_location
+    df.dropna(subset=['user_location'],inplace=True)
+
+    ###### COUNTRY SEMANTICS #######
+    # adding all data that include 'Canada' in user_location to the new dataframe
+    temp_df = temp_df.append(df[df['user_location'].str.contains('canada')])
+
+    ##### PROVINCE ANALYSIS ########
+    # Using the full name of the province + internationally approved abbreviations
+
+    ## NEWFOUNDLAND AND LABRADOR ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('newfoundland and labrador')])
 
 
-## NOVA SCOTIA ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('nova scotia')])
-
-## NEW BRUNSWICK ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('new brunswick')])
+    ## PRINCE EDWARD ISLAND ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('prince edward island')])
 
 
-## QUEBEC ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('quebec')])
+    ## NOVA SCOTIA ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('nova scotia')])
 
-## ONTARIO ##
-
-temp_df = temp_df.append(df[df['user_location'].str.contains('ontario')])
-
-
-## MANITOBA ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('manitoba')])
+    ## NEW BRUNSWICK ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('new brunswick')])
 
 
-## SASKATCHEWAN ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('saskatchewan')])
+    ## QUEBEC ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('quebec')])
 
-## ALBERTA ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('alberta')])
+    ## ONTARIO ##
 
-## BRITISH COLUMBIA ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('british columbia')])
+    temp_df = temp_df.append(df[df['user_location'].str.contains('ontario')])
 
 
-## YUKON ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('yukon')])
-
-## NORTHWEST TERRITORIES ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('northwest territories')])
-
-## NUNAVUT ##
-temp_df = temp_df.append(df[df['user_location'].str.contains('nunavut')])
-
-################# CITY ANALYSIS ##################
-cities = pd.read_csv("Cities/cities.csv", engine='python')
-# make all text lower case
-cities.Name = cities.Name.str.lower()
-# dropping all NaN values
-cities.dropna(subset=['Name'],inplace=True)
-
-for city in cities.Name:
-    # checks to see if any user location starts with or ends with the city
-    temp_df = temp_df.append(df[df['user_location'].str.startswith(city)])
-    temp_df = temp_df.append(df[df['user_location'].str.endswith(city)])
+    ## MANITOBA ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('manitoba')])
 
 
+    ## SASKATCHEWAN ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('saskatchewan')])
 
-# creating a separate dataframe for retweets
-rt_tweets_df = pd.DataFrame()
+    ## ALBERTA ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('alberta')])
 
-rt_tweets_df = rt_tweets_df.append(temp_df[temp_df['text'].str.contains('rt @')])
-og_tweets_df = temp_df[temp_df['text'].str.contains('rt @') == False]
+    ## BRITISH COLUMBIA ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('british columbia')])
 
-# exporting files with different names each time based on the year, month, and day
-import time
-timestr = time.strftime("%Y%m%d-%H%M%S")
 
-rt_tweets_df.to_csv('./clean_data/retweets-{}.csv'.format(timestr))
-print("Dimensions of retweets: {}".format(rt_tweets_df.shape))
+    ## YUKON ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('yukon')])
 
-og_tweets_df.to_csv('./clean_data/og_tweets-{}.csv'.format(timestr))
-print("Dimensions of original: {}".format(og_tweets_df.shape))
+    ## NORTHWEST TERRITORIES ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('northwest territories')])
+
+    ## NUNAVUT ##
+    temp_df = temp_df.append(df[df['user_location'].str.contains('nunavut')])
+
+    ################# CITY ANALYSIS ##################
+    cities = pd.read_csv("Cities/cities.csv", engine='python')
+    # make all text lower case
+    cities.Name = cities.Name.str.lower()
+    # dropping all NaN values
+    cities.dropna(subset=['Name'],inplace=True)
+
+    for city in cities.Name:
+        # checks to see if any user location starts with or ends with the city
+        temp_df = temp_df.append(df[df['user_location'].str.startswith(city)])
+        temp_df = temp_df.append(df[df['user_location'].str.endswith(city)])
+
+
+
+    # creating a separate dataframe for retweets
+    rt_tweets_df = pd.DataFrame()
+
+    rt_tweets_df = rt_tweets_df.append(temp_df[temp_df['text'].str.contains('rt @')])
+    og_tweets_df = temp_df[temp_df['text'].str.contains('rt @') == False]
+
+    # exporting files with different names each time based on the year, month, and day
+    import time
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+
+    rt_tweets_df.to_csv('./clean_data/retweets-{}.csv'.format(timestr))
+    print("Dimensions of retweets: {}".format(rt_tweets_df.shape))
+
+    og_tweets_df.to_csv('./clean_data/og_tweets-{}.csv'.format(timestr))
+    print("Dimensions of original: {}\n".format(og_tweets_df.shape))
+
+def main():
+    while True:
+        filename = input("Enter the file(s) that you wish to be cleaned (press q to exit): ")
+
+        if filename == "q":
+            break
+
+        filename = filename.split(" ")
+        counter = 0
+        while counter < len(filename):
+            file = "./Data/"
+            file = file + filename[counter]
+
+            try:
+                open(file)
+                print("Cleaning {}...\n".format(filename[counter]))
+                clean_data(file)
+            except:
+                print("File not found try again")
+                break
+
+            counter+=1
+
+
+
+main()
